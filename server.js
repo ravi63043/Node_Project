1e57c390dcd51a6bffb367bb16447187735ebe17
ravi <ravi105763@gmail.com>
2021-07-15 15:25:17 +0530

    Intail Author Routes

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
+if(process.env.NODE_ENV!=='production')
+{
+    require('dotenv').config();
+}
 const express=require('express');
 const app=express();
+app.use(express.urlencoded({ extended: false }));
 const expressLayouts=require('express-ejs-layouts');
 const indexRouter=require('./routes/index')
+const authorRouter=require('./routes/authors')
 app.set('view engine','ejs');
 app.set('views',__dirname + '/views');
 app.set('layout','layouts/layout');
+app.use(expressLayouts)
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use('/',indexRouter);
+app.use('/authors',authorRouter);
 const mongoose=require('mongoose');
-mongoose.connect(process.env.DATABASE_URL,{useNewUrlparser:true})
+mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true,useUnifiedTopology: true})
 .then(()=>console.log(`Mongo DB connected`))
 .catch(()=>console.log(`Mongo DB not connected`))
 const port=process.env.PORT||3000;
